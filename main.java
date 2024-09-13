c709e833795bf2efbe18fe07a0e90f4d58666e8a
jjulioamorim <julioamorim159@gmail.com>
2024-09-13 19:23:51 -0300

    novo sist

---
+++ main.java
@@ -0,0 +1,68 @@
+class Tarefa {
+    // Atributos privados
+    private String titulo;
+    private String descricao;
+    private String prazo;
+    private int prioridade;
+
+    // Construtor
+    public Tarefa(String titulo, String descricao, String prazo, int prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prazo = prazo;
+        this.prioridade = prioridade;
+    }
+
+    // construtor 2: construtor com menos  parâmetros
+        public tarefa(String titulo, String prazo){
+            this.titulo = titulo;
+            this.prazo = prazo;
+        }
+
+    // Métodos públicos para acessar os atributos
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPrazo() {
+        return this.prazo;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    
+    public void exibirDetalhes() {
+        System.out.println("Título: " + this.titulo);
+        System.out.println("Descrição: " + this.descricao);
+        System.out.println("Prazo: " + this.prazo);
+        System.out.println("Prioridade: " + this.prioridade);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Tarefa tarefa1 = new Tarefa("Estudar POO", "Revisar os conceitos de classes e objetos", "2024-09-05", 1);
+
+        System.out.println("=============== Testando métodos gets ===============");
+        System.out.println("Título: " + tarefa1.getTitulo());
+        System.out.println("Descrição: " + tarefa1.getDescricao());
+        System.out.println();
+
+        System.out.println("=============== Imprimindo com método exibitDetalhes() ===============");
+        tarefa1.exibirDetalhes();
+    }
+}
